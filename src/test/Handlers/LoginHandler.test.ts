169f3d2e23855386ad7fdb3aeabdea1a21380b5a
Chloe <chloee@ChloeuicBookAir.cg.shawcable.net>
2022-04-03 20:04:52 -0600

    mock introduction: jest.fn, creation, injection

---
+++ src/test/Handlers/LoginHandler.test.ts
@@ -0,0 +1,31 @@
+import { isJSDocUnknownTag } from 'typescript';
+import { LoginHandler } from '../../app/Handlers/LoginHandler'
+import { HTTP_CODES, HTTP_METHODS } from '../../app/Models/ServerModels';
+
+
+describe('LoginHandler test suite', () => {
+  let logingHandler: LoginHandler;
+
+  const requestMock = {
+    method: ''
+  };
+  const responseMock = {
+    writeHead: jest.fn()
+  };
+  const authorizerMock = {};
+
+  beforeEach(() => {
+    logingHandler = new LoginHandler(
+      requestMock as any,
+      responseMock as any,
+      authorizerMock as any
+    )
+  })
+
+  test('options request', async () => {
+    requestMock.method = HTTP_METHODS.OPTIONS;
+    await logingHandler.handleRequest();
+    expect(responseMock.writeHead).toBeCalledWith(HTTP_CODES.OK)
+  })
+
+})
